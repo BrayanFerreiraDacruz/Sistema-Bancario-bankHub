7122cba8940b17f6be08a3e0aa31bc0ffce3b46a
Brayan Ferreira Da Cruz <brayanferreiradacruz0@gmail.com>
2026-01-08 11:52:58 -0300

    Security: removendo credenciais do codigo e usando variaveis de ambiente

--- conexaobanco/Conexao.java
+++ conexaobanco/Conexao.java
@@ -6,17 +6,20 @@ import java.sql.SQLException;
 
 public class Conexao {
 
+    // Pegamos os valores do Sistema Operacional, não do código escrito
     private static final String URL = "jdbc:postgresql://localhost:5432/SistemaBancario";
-    private static final String USUARIO = "postgres";
-    private static final String SENHA = "147604";
+    private static final String USUARIO = System.getenv("DB_USER");
+    private static final String SENHA = System.getenv("DB_PASSWORD");
 
     public static Connection getConexao() {
         try {
-
+            // Se as variáveis não estiverem configuradas, avisar
+            if (USUARIO == null || SENHA == null) {
+                throw new SQLException("Variáveis de ambiente DB_USER ou DB_PASSWORD não encontradas!");
+            }
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (SQLException e) {
             System.err.println("❌ Erro de conexão: " + e.getMessage());
-
             return null;
         }
     }
